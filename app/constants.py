3f52954f51ac9ef7a654671437f96e323b6ba99d
Jonnathan <45534456+JonnHenry@users.noreply.github.com>
2025-04-10 01:29:43 -0500

    Actualizar el modelo de predicción

--- app/constants.py
+++ app/constants.py
@@ -1,12 +1,17 @@
 PATH_ENCODER = '../../model_ml/one_hot_enconder.pkl'
 PATH_SCALER = '../../model_ml/robust_escaler.pkl'
-PATH_MODEL_ML = '../../model_ml/logistic_regresion.pkl'
+PATH_MODEL_ML = '../../model_ml/xgb.pkl'
 
 
 #data information
 numerical_columns = ['device_fraud_count','income','name_email_similarity','prev_address_months_count','current_address_months_count','customer_age','days_since_request','intended_balcon_amount','zip_count_4w','velocity_6h','velocity_24h','velocity_4w','bank_branch_count_8w','date_of_birth_distinct_emails_4w','credit_risk_score','bank_months_count','proposed_credit_limit','session_length_in_minutes','device_distinct_emails_8w','month']
 categorical_columns = ['payment_type', 'employment_status', 'housing_status', 'source', 'device_os']
 target_var = 'fraud_bool'
-features_model = ['device_os_windows', 'housing_status_BA', 'month', 'credit_risk_score','customer_age', 'income', 'payment_type_AC',
-                'employment_status_CC', 'date_of_birth_distinct_emails_4w', 'has_other_cards', 'email_is_free', 'employment_status_CA', 
-                'device_os_linux', 'proposed_credit_limit','current_address_months_count', 'bank_months_count']
+features_model = ['payment_type_AC', 'proposed_credit_limit', 'customer_age',
+       'housing_status_BC', 'date_of_birth_distinct_emails_4w',
+       'name_email_similarity', 'credit_risk_score', 'email_is_free',
+       'device_os_linux', 'zip_count_4w', 'device_os_windows',
+       'has_other_cards', 'device_os_other', 'income', 'employment_status_CA',
+       'bank_months_count', 'housing_status_BE', 'housing_status_BA',
+       'bank_branch_count_8w', 'keep_alive_session', 'housing_status_BB',
+       'current_address_months_count', 'phone_home_valid']
--- app/controllers/prediction_controller.py
+++ app/controllers/prediction_controller.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter , HTTPException, status
 from app.models.models import PredictionInput,PredictionOutput
 from app.utils.utils import PrepareData, PredictionModel
+from fastapi.responses import RedirectResponse
 
 router = APIRouter()
 prepare_data = PrepareData()
 prediction_model = PredictionModel()
 
 @router.post('/fraud/predict',status_code=status.HTTP_200_OK,response_model=PredictionOutput)
-def create_stroke(pred_input: PredictionInput):
+def predict_BAF(pred_input: PredictionInput):
     try:
         df_pred_input = prepare_data.transform_data_to_predict(pred_input)
         result_prediction = prediction_model.load_model_ml().predict(df_pred_input)
@@ -18,3 +19,8 @@ def create_stroke(pred_input: PredictionInput):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Ocurrió un error inesperado: {str(e)}"
         )
+    
+
+@router.get("/",include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
